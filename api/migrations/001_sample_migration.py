f5921ac72761339eae1c783916626d9a5c881580
Shujin Pang <shujin.pang17@gmail.com>
2023-08-16 00:46:57 +0000

    #31 Startup and insert userdata

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -2,37 +2,80 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+        CREATE TABLE accounts (
+            id SERIAL NOT NULL PRIMARY KEY,
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            username VARCHAR(100) NOT NULL UNIQUE,
+            hashed_password TEXT NOT NULL,
+            phone BIGINT,
+            zip INT NOT NULL
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
-        """
+        DROP TABLE accounts;
+        """,
     ],
     [
         # "Up" SQL statement
         """
-        CREATE TABLE big_dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+        CREATE TABLE pets (
+            id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+            pet_name VARCHAR(100) NOT NULL,
+            picture TEXT,
+            age SMALLINT,
+            species VARCHAR(100),
+            breed VARCHAR(100),
+            color VARCHAR(100),
+            weight INTEGER,
+            disease TEXT,
+            medication TEXT,
+            allergy TEXT,
+            dietary_restriction TEXT,
+            description TEXT,
+            owner_id INTEGER REFERENCES accounts("id") ON DELETE CASCADE
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE big_dummy;
+        DROP TABLE pets;
+        """,
+    ],
+    [
+        # "Up" SQL statement
         """
-    ]
+        CREATE TABLE donations (
+            id SERIAL NOT NULL PRIMARY KEY,
+            item_name VARCHAR(100) NOT NULL,
+            description TEXT,
+            date DATE NOT NULL,
+            picture TEXT,
+            category VARCHAR(100) NOT NULL,
+            claimed BOOLEAN NOT NULL,
+            owner_id INTEGER NOT NULL REFERENCES accounts("id") ON DELETE CASCADE
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE donations;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE vaccines (
+            id SERIAL NOT NULL PRIMARY KEY,
+            pet_id INTEGER NOT NULL REFERENCES pets("id") ON DELETE CASCADE,
+            vaccine_name VARCHAR(100) NOT NULL,
+            clinic TEXT,
+            received_date DATE,
+            due_date DATE
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE vaccines;
+        """,
+    ],
 ]
